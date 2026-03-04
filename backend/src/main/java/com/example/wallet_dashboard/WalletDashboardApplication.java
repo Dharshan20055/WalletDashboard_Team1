a2620815dcd2f4ac5f6797138dd162dee2fbd5bf
Dharshan20055 <dharshanmurali05@gmail.com>
2026-03-04 19:41:58 +0530

    Created the folder structure

---
+++ backend/src/main/java/com/example/wallet_dashboard/WalletDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.wallet_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletDashboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/wallet_dashboard/WalletDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wallet_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
